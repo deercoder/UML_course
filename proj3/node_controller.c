99d2e3a84dec106162593c31fda59308f1039434
Liu Chang <lllccc2009@gmail.com>
2014-11-30 20:02:09 -0500

    Add more details about server/client design

--- proj3/node_controller.c
+++ proj3/node_controller.c
@@ -4,6 +4,7 @@
 
 #include "buf_mgr.h"
 #include "ddonuts.h"
+#include "global.h"
 
 #include <unistd.h>
 #include <pthread.h>
@@ -27,6 +28,8 @@ int connected_ch[MAXNODE];
 void *chan_monitor(void *);
 void *msg_maker(void *);
 
+void child_handler(int signum);
+
 
 /*
  *  establish the node controller
@@ -34,8 +37,24 @@ void *msg_maker(void *);
  */
 int main(int argc, char **argv)
 {
-   int role;
-   int number;
+    // node id & total node number
+    int role;
+    int number;
+
+    // data for client-end socket
+    int inet_sock_client;
+    struct hostent *heptr_client;
+    struct sockaddr_in inet_telnum_client;	
+
+    // data for server-end socket
+    int inet_sock_server;
+    struct hostent *heptr_server;
+    struct sockaddr_in inet_telnum_server;
+    int wild_card = INADDR_ANY;
+    sigset_t mask;
+    struct sigaction sigstrc;
+
+    // data
 
    if (argc != 3){
 	printf("Error: input number mismatch!\n");
@@ -48,7 +67,33 @@ int main(int argc, char **argv)
    // set up client for this node(except for last node without client) 
    if (role != number)
    {
+       // allocate a socket to communicate with
+       if ((inet_sock_client=socket(AF_INET, SOCK_STREAM, 0)) == -1) {
+	   perror("inet_socke_client allocation failed: ");
+     	   exit(1);
+       }
 
+       // get a host pointer to point to a hostent structure
+       // which contains the remote IP address of server
+       if ((heptr_client = gethostbyname(host_list[role])) == NULL) {
+	   perror("gethostbyname failed: ");
+	   exit(1);
+       }
+
+       // byte copy the IP address from the h_addr field in the hostend
+       // structure into an IP address structure
+       bcopy(heptr_client->h_addr, &inet_telnum_client.sin_addr, heptr_client->h_length);
+       inet_telnum_client.sin_family = AF_INET;
+       inet_telnum_client.sin_port = htons((u_short) PORT);	
+
+       // use the connect system call to open a TCP connection
+       if (connect(inet_sock_client, (struct sockaddr *)&inet_telnum_client, sizeof(struct sockaddr_in)) == -1) {
+		perror("inet_sock_client connect failed: ");
+		exit(1);
+	}
+
+	
+	// remain blank for other operations in the client-end
 
    } 
  
@@ -56,8 +101,34 @@ int main(int argc, char **argv)
    // set up server for this node(except for 1st node without server)
    if (role != 1)
    {
+	// set up sigaction structure to clean zombies
+	sigemptyset(&mask);
+	sigstrc.sa_handler = child_handler;
+	sigstrc.sa_mask = mask;
+	sigstrc.sa_flags = 0;
+	sigaction(SIGCHLD, &sigstrc, NULL);
 
+	// allocate a socket to communicate with
+	if ((inet_sock_server = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
+		perror("inet_sock_server allocation failed:");
+		exit(1);
+	}
+	
+	// byte copy the wild_card IP address INADDR_ANY into
+	// IP address structure, along with the port and family add
+	// use the structure to give yourself a connect address
+	bcopy(&wild_card, &inet_telnum_server.sin_addr, sizeof(int));
+	inet_telnum_server.sin_family = AF_INET;
+	inet_telnum_server.sin_port = htons( (u_short) PORT);	
 
+	// bind to the IP address
+	if (bind(inet_sock_server, (struct sockaddr *)&inet_telnum_server, sizeof(struct sockaddr_in)) == -1) {
+		perror("inet_sock bind failed:");
+		exit(2);
+	}
+	
+	// allow client connect requests to arrive: call-wait 5
+	listen(inet_sock_server, 5);
 
    }
 
@@ -69,4 +140,6 @@ int main(int argc, char **argv)
 }
 
 
-
+void child_handler(int signum) {
+	wait(NULL);
+}
