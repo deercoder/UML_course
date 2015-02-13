a267e88d5bc0c761f6560bff2382c7f9845ed55f
Liu Chang <lllccc2009@gmail.com>
2015-02-13 15:16:38 -0500

    Add homework1 for ML.
    Note that source code is missing, but it is easy to complete

---
+++ ML/ps1/ex1 starter code/warmUpExercise.m
@@ -0,0 +1,22 @@
+function A = warmUpExercise()
+
+%   A = WARMUPEXERCISE() is an example function that returns a 5x5 matrix of ones
+
+A = [];
+% ============= YOUR CODE HERE ==============
+% Instructions: Return a 5x5 matrix of ones
+%               In octave, we return values by defining which variables
+%               represent the return values (at the top of the file)
+%               and then set them accordingly. 
+% Hint: This exercise is pretty straightforward, all you need to do is 
+% adding "A = ones(5);" in the blank area below.  
+
+
+
+
+
+
+% ===========================================
+
+
+end
